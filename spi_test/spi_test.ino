23a2c6faa58686f6b00ef76e6e4d14420025628f
onibrow <seiya.ono12@gmail.com>
2019-02-06 01:32:37 +0000

    get code to work with adas1000

--- spi_test/spi_test.ino
+++ spi_test/spi_test.ino
@@ -8,25 +8,26 @@ Author: Seiya Ono
 #include <SPI.h>
 
 //ADAS1000's memory register addresses:
-const char ECGCTL = 0x01;     // ECG control // reset value 0x000000
-const char LOFFCTL = 0x02;    // Lead-off control 
-const char PACEAMPTH = 0x07;  // Show PACEAMPTH or Pace amplitude threshold // reset value 0x242424
+const char NOP       = 0x00;
+const char ECGCTL    = 0x01;   // ECG control // reset value 0x000000
+const char FRMCTL    = 0x0A;   // Controls what shows up in FRAME
+const char LOFFCTL   = 0x02;   // Lead-off control 
+const char PACEAMPTH = 0x07;   // Show PACEAMPTH or Pace amplitude threshold // reset value 0x242424
 
-const char LADATA = 0x11;     // LA or Lead I data 
-const char LLDATA = 0x12;     // LL or Lead II data
-const char RADATA = 0x13;     // RA or Lead III data
-const char V1DATA = 0x14;     // V1 or V1’ data
-const char V2DATA = 0x15;     // V2 or V2’ data
-const char PACEDATA = 0x1A;   // Read pace detection data/status
-
-const char FRAMES = 0x40;     // Frame header // reset value 0x800000
+const char LADATA    = 0x11;   // LA or Lead I data 
+const char LLDATA    = 0x12;   // LL or Lead II data
+const char RADATA    = 0x13;   // RA or Lead III data
+const char V1DATA    = 0x14;   // V1 or V1’ data
+const char V2DATA    = 0x15;   // V2 or V2’ data
+const char PACEDATA  = 0x1A;   // Read pace detection data/status
+const char FRAMES    = 0x40;   // Frame header // reset value 0x800000
 
 // pins used for the connection with the AFE
 const int CS = 18;
 
-unsigned char values[10];
+unsigned char buff[4];
 
-bool DEBUG = true;
+bool DEBUG = false;
 
 void setup() {
   Serial.begin(9600);
@@ -34,6 +35,7 @@ void setup() {
   // start the SPI library:
   SPI.begin();
   SPI.setDataMode(SPI_MODE3);
+  SPI.setClockDivider(SPI_CLOCK_DIV32);
   // initialize the  data ready and chip select pins:
   pinMode(CS, OUTPUT);
   digitalWrite(CS, HIGH);
@@ -42,7 +44,11 @@ void setup() {
   
   delay(50);
   // Control Register
+  readRegister(NOP);
+  writeRegister(FRMCTL, 0x07FC00);
+  readRegister(FRMCTL);
   writeRegister(ECGCTL, 0xF800BE);
+  readRegister(ECGCTL);
   writeRegister(LOFFCTL, 0x000000);
   writeRegister(PACEAMPTH, 0x242424);
   Serial.println("Setup Complete!\n");
@@ -52,56 +58,39 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(CS, LOW);
-  unsigned payload = PACEAMPTH << 24;
-//  Serial.print("Sending 0x"); Serial.println(payload, HEX);
-  Serial.print("Sending 0x"); Serial.println(0x00000000, HEX);
-  int val = SPI.transfer(payload);
-  Serial.print("Read 0x"); Serial.println(val, HEX);
-  digitalWrite(CS, HIGH);
-  delay(1000);
+  readRegister(NOP);
+  delay(0.5);
 }
 
-//void readRegister(char registerAddress, int numBytes, unsigned char * values) {
-//  //Since we're performing a read operation, the most significant bit of the register address should be set.
-//  char address = 0x00 | registerAddress; // 0x08-->0x00
-//  //If we're doing a multi-byte read, bit 6 needs to be set as well.
-//  if (numBytes > 1)address = address | 0x40;
-//  
-//  //Set the Chip select pin low to start an SPI packet.
-//  digitalWrite(CS, LOW);
-//  //Transfer the starting register address that needs to be read.
-//  SPI.transfer(address);
-//  //Continue to read registers until we've read the number specified, storing the results to the input buffer.
-//  for (int i = 0; i < numBytes; i++) {
-//  values[i] = SPI.transfer(0x00);
-//  }
-//  //Set the Chips Select pin high to end the SPI packet.
-//  digitalWrite(CS, HIGH);
-//}
-
 void readRegister(byte reg) {
-  // R/W:  [31]
-  // Reg:  [30:24]
-  // Data: [23:0]
+  /* R/W:  [31]
+     Reg:  [30:24]
+     Data: [23:0] */
   unsigned int payload = (0b0 << 31) | (reg << 24) | 0x000000;
   if (DEBUG) {Serial.print("Reading 0x"); Serial.println(payload, HEX);}
-  // transfer payload
+  
+  // read out
   digitalWrite(CS, LOW);
-  SPI.transfer(payload);
+  for (int i = 0; i < 4; i++) {
+    char c = (payload >> (24 - (8 * i))) & 0xFF;
+    buff[i] = SPI.transfer(c);
+  }
   digitalWrite(CS, HIGH);
 }
 
 
 // Write to ADAS1000
 void writeRegister(byte reg, int val) {
-  // R/W:  [31]
-  // Reg:  [30:24]
-  // Data: [23:0]
+  /* R/W:  [31]
+     Reg:  [30:24]
+     Data: [23:0] */
   unsigned int payload = (0b1 << 31) | (reg << 24) | (val & 0x00FFFFFF);
   if (DEBUG) {Serial.print("Writing 0x"); Serial.println(payload, HEX);}
   // transfer payload
   digitalWrite(CS, LOW);
-  SPI.transfer(payload);
+  for (int i = 0; i < 4; i++) {
+    char c = (payload >> (24 - (8 * i))) & 0xFF;
+    SPI.transfer(c);
+  }
   digitalWrite(CS, HIGH);
 }
