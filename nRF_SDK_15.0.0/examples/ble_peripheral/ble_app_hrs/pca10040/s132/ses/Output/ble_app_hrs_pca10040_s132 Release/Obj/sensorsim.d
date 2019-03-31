9821472eea4b70cda9e6487c1022943fc9587e70
onibrow <seiya.ono12@gmail.com>
2019-03-31 02:24:32 +0000

    add nrf sdk

---
+++ nRF_SDK_15.0.0/examples/ble_peripheral/ble_app_hrs/pca10040/s132/ses/Output/ble_app_hrs_pca10040_s132 Release/Obj/sensorsim.d
@@ -0,0 +1,5 @@
+Output/ble_app_hrs_pca10040_s132\ Release/Obj/sensorsim.o: \
+ /home/onibrow/Documents/nRF5_SDK_15.0.0_a53641a/components/libraries/sensorsim/sensorsim.c \
+ /home/onibrow/Documents/nRF5_SDK_15.0.0_a53641a/components/libraries/sensorsim/sensorsim.h \
+ /usr/share/segger_embedded_studio_for_arm_4.12/include/stdint.h \
+ /usr/share/segger_embedded_studio_for_arm_4.12/include/stdbool.h
---
+++ nRF_SDK_15.0.0/examples/ble_peripheral/ble_app_hrs/pca10040/s132/ses/Output/ble_app_hrs_pca10040_s132 Release/Obj/ses_nrf52_Vectors.d
@@ -0,0 +1,2 @@
+Output/ble_app_hrs_pca10040_s132\ Release/Obj/ses_nrf52_Vectors.o: \
+ /home/onibrow/Documents/nRF5_SDK_15.0.0_a53641a/modules/nrfx/mdk/ses_nrf52_Vectors.s
