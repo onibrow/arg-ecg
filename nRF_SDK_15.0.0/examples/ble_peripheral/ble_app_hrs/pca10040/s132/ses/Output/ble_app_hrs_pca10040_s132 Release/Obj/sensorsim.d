0013172f08f8d95209e8a4c19ba9f645bd74e11a
onibrow <seiya.ono12@gmail.com>
2019-04-01 23:26:20 +0000

    remove sdk

--- nRF_SDK_15.0.0/examples/ble_peripheral/ble_app_hrs/pca10040/s132/ses/Output/ble_app_hrs_pca10040_s132 Release/Obj/sensorsim.d
+++
@@ -1,5 +0,0 @@
-Output/ble_app_hrs_pca10040_s132\ Release/Obj/sensorsim.o: \
- /home/onibrow/Documents/nRF5_SDK_15.0.0_a53641a/components/libraries/sensorsim/sensorsim.c \
- /home/onibrow/Documents/nRF5_SDK_15.0.0_a53641a/components/libraries/sensorsim/sensorsim.h \
- /usr/share/segger_embedded_studio_for_arm_4.12/include/stdint.h \
- /usr/share/segger_embedded_studio_for_arm_4.12/include/stdbool.h
--- nRF_SDK_15.0.0/examples/ble_peripheral/ble_app_hrs/pca10040/s132/ses/Output/ble_app_hrs_pca10040_s132 Release/Obj/ses_nRF_Startup.d
+++
@@ -1,2 +0,0 @@
-Output/ble_app_hrs_pca10040_s132\ Release/Obj/ses_nRF_Startup.o: \
- /home/onibrow/Documents/nRF5_SDK_15.0.0_a53641a/modules/nrfx/mdk/ses_nRF_Startup.s
